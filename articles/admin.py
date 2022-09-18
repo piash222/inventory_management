ec7a8d57734f0a835ce69398819ff4071c582b2f
Md. Shihab Uddin <piash222@gmail.com>
2022-09-18 11:58:40 +0600

    configure article model with admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
+
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pk', 'title']
+    search_fields = ['title', 'content']
+    # search_help_text = "abra ka dabra"
+
+
+admin.site.register(Article, ArticleAdmin)
