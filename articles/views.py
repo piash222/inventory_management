6cdeb6051a5331c32c117227b78aeaa1cfaad14d
Md. Shihab Uddin <piash222@gmail.com>
2022-09-18 10:49:58 +0600

    article list and detail views

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Article
+
 
 # Create your views here.
+def article_list(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/home_view.html', context)
+
+
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/article_detials.html', context)
