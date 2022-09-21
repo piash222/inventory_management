8652cb976f172ce350a6dda348638820585a5359
Md. Shihab Uddin <piash222@gmail.com>
2022-09-21 15:51:25 +0600

    Create a Login View to Authenticate Users

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
+    path('', include("articles.urls")),
     path('admin/', admin.site.urls),
-    path('', include("articles.urls"))
+    path('login/', login_view),
+
 ]
