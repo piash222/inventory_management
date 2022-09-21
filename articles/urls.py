0bb8ce6b58c0825b4f4405a285192e2e5161b552
Md. Shihab Uddin <piash222@gmail.com>
2022-09-21 10:40:33 +0600

    Search Form & Request Data

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, article_search_view
 
 urlpatterns = [
     path('', article_list),
+    path('article/', article_search_view),
     path('article/<int:pk>', article_detail)
 ]
--- articles/views.py
+++ articles/views.py
@@ -11,6 +11,21 @@ def article_list(request):
     return render(request, 'articles/home_view.html', context)
 
 
+def article_search_view(request):
+    query_dict = request.GET
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+    article = None
+    if query is not None:
+        article = Article.objects.get(pk=query)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/article_search.html', context)
+
+
 def article_detail(request, pk):
     article = Article.objects.get(pk=pk)
     context = {
