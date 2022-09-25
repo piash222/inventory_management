4ad36ece42fd82381402a94ac25701bd29b3d83d
Md. Shihab Uddin <piash222@gmail.com>
2022-09-25 11:32:10 +0600

    Login via Django AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
 def login_view(request):
+
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "invalid username or password"}
-            return render(request, 'accounts/login.html', context)
-        print(user)
-        login(request, user)
-        return redirect('/')
-    return render(request, 'accounts/login.html')
+        form = AuthenticationForm(request, data=request.POST)
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        # user = authenticate(request, username=username, password=password)
+        # if user is None:
+        #     context = {"error": "invalid username or password"}
+        #     return render(request, 'accounts/login.html', context)
+        # print(user)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout_view(request):
